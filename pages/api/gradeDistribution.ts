ae0739f2ceb4052e056e9c77941d0f5e1a7626f4
Charles O'Hanlon <charlescohanlon@gmail.com>
2022-07-07 21:31:46 -0700

    prep backend stuff

--- pages/api/gradeDistribution.ts
+++ pages/api/gradeDistribution.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string
-}
+// type Data = {
+//   name: string;
+// };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const result = await queryDb();
+//     res.status(200).json(result);
+//   } catch (err) {
+//     res.status(500).json({ error: "failed to load data" });
+//   }
+// }
